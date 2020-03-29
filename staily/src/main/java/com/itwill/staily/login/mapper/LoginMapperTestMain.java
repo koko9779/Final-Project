53fdbf90abda347807ca5e7dece8473290e41eb1
yeongju-chun <yeongju30@naver.com>
2020-03-29 10:27:55 +0900

    loginMapper, SignMapper 수정하고 테스트까지 완료!

--- staily/src/main/java/com/itwill/staily/login/mapper/LoginMapperTestMain.java
+++ staily/src/main/java/com/itwill/staily/login/mapper/LoginMapperTestMain.java
@@ -1,10 +1,9 @@
 package com.itwill.staily.login.mapper;
 
+import java.util.HashMap;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.itwill.staily.stylecoodination.mapper.BoardManageMapper;
-import com.itwill.staily.util.Board;
 
 public class LoginMapperTestMain {
 
@@ -12,14 +11,26 @@ public class LoginMapperTestMain {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/application-config.xml");
 		LoginMapper login = applicationContext.getBean(LoginMapper.class);
 		int count;
+		HashMap<String, Object> map = new HashMap<String, Object>();
 
+//		Member member = login.selectMemberIdAndPw("ehdanf91");
+//		System.out.println(member);
 		
+
+//		map.put("mPhone", "01091434908");
+//		map.put("mName", "천영주");
+//		String id = login.selectIdOne(map);
+//		System.out.println(id);
 		
+//		map.put("mPhone", "112");
+//		map.put("mId", "hiphopmy");
+//		count = login.isIdExist(map);
+//		System.out.println(count);
 		
-		
-		
-		
-		
+//		map.put("mPw", "1234");
+//		map.put("mId", "ehdanf91");
+//		count = login.updatePwOne(map);
+//		System.out.println(count);
 		
 		
 		
