f920f8a1a1e0732a799458382e99933281ff0992
yeongju-chun <yeongju30@naver.com>
2020-03-31 19:45:48 +0900

    service 추가구현

--- staily/src/main/java/com/itwill/staily/stylecoodination/service/StyleCoodinationServiceImpl.java
+++ staily/src/main/java/com/itwill/staily/stylecoodination/service/StyleCoodinationServiceImpl.java
@@ -22,6 +22,11 @@ public class StyleCoodinationServiceImpl {
 	@Autowired
 	private ViewMapper viewMapper;
 	
+	public int modifyBoardAndReply() {
+		
+		return 0;
+	}
+	
 	public int writeBoard(Board board) {
 		int writeCount = 0;
 		
@@ -59,7 +64,31 @@ public class StyleCoodinationServiceImpl {
 		return writeCount;
 	}
 	
-	public int removeReply() {
+	public int removeReply(int bNo) {
+		int removeCount = 0;
+		
+		removeCount = replyManageMapper.deleteReply(bNo);
+		if(removeCount != 1) {
+			throw new FailRemoveBoardException("게시글 삭제에 실패하였습니다");
+		}
+		return removeCount;
+	}
+	
+	public int checkChoice(int bNo) {
+		int updateCount = 0;
+		
+		updateCount = replyManageMapper.updateChoice(bNo);
+		//업데이트 안되면 0인 상태로 controller에서 
+		//ui에 영향을 미칠 데이터 작업
+		return updateCount;
+	}
+	
+	public int checkRecommend(int bNo) {
+		int updateCount = 0;
+		
+		updateCount= replyManageMapper.updateRecommend(bNo);
+		//업데이트 안되면 0인 상태로 controller에서 
+		//ui에 영향을 미칠 데이터 작업
 		return 0;
 	}
 	
