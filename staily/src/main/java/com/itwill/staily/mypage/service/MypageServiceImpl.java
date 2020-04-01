abcf758f5934d195584bb67d2075ef24244fa9c3
growworld <growworld@naver.com>
2020-04-01 17:08:27 +0900

    12312312

--- staily/src/main/java/com/itwill/staily/mypage/service/MypageServiceImpl.java
+++ staily/src/main/java/com/itwill/staily/mypage/service/MypageServiceImpl.java
@@ -1,9 +1,13 @@
 package com.itwill.staily.mypage.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.itwill.staily.mypage.mapper.MypageMapper;
+import com.itwill.staily.util.Company;
 import com.itwill.staily.util.Member;
+import com.itwill.staily.util.Product;
 
 public class MypageServiceImpl implements MypageService {
 	@Autowired
@@ -27,6 +31,24 @@ public class MypageServiceImpl implements MypageService {
 	@Override
 	public Member selectMemberCompany(int mNo) throws Exception {
 		return mypageMapper.selectMemberCompany(mNo);
+	}
+
+	@Override
+	public boolean updateCompanyNo(Company company) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Product> selectWriteList(int mNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteWrite(int pNo) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
