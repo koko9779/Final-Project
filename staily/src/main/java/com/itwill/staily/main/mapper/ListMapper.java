4fbdca7d56ead0195b9f54eac121019bc767e0a5
ujandgd@naver.com <ujandgd@naver.com>
2020-04-01 20:40:41 +0900

    MainMapperTest, ListMapperTest (완료)

--- staily/src/main/java/com/itwill/staily/main/mapper/ListMapper.java
+++ staily/src/main/java/com/itwill/staily/main/mapper/ListMapper.java
@@ -5,15 +5,18 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.itwill.staily.util.Product;
 import com.itwill.staily.util.Work;
 
 @Mapper
 public interface ListMapper {
 	/*
- 		1.작품상세페이지 작품정보(작품이름,작품 포스터) 출력
+ 		//WorkDetailMapper
+ 		1.작품상세페이지 작품정보(작품이름,작품 포스터) 출력 
+ 		
 		2.작품상세페이지 상품정보(상품이름, 상품에피소드,상품씬,작성자) 출력
-		3.작품상세페이지 상품정보 회차별 출력하기
-		
+		3.작품상세페이지 상품정보 회차별 출력
+				
 		4.즐겨찾기 등록	
 		5.즐겨찾기 제거
 		6.게시물 총 건수
@@ -22,9 +25,9 @@ public interface ListMapper {
 		- 상품들을 인기순으로 정렬하기
 	 */
 
-	public Work selectByWork(int wNo);
-	public List<Work> selectByEpisode(Map map);
-	
+	public List<Work> selectProductList(int wNo);
+	public List<Work> selectProductListByEpisode(Map map);
+		
 	public int createBookmark(Map map);
 	public int deleteBookmark(int bmNo);
 	public int selectProductCount();
--- staily/src/main/java/com/itwill/staily/main/mapper/ListMapperTest.java
+++ staily/src/main/java/com/itwill/staily/main/mapper/ListMapperTest.java
@@ -1,70 +1,92 @@
 package com.itwill.staily.main.mapper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.itwill.staily.detail.mapper.ProductDetailMapper;
+import com.itwill.staily.detail.mapper.WorkDetailMapper;
+import com.itwill.staily.util.Product;
+import com.itwill.staily.util.Work;
 
 public class ListMapperTest {
+
+	public static void main(String[] args) throws Exception {
 	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/application-config.xml");
 	ListMapper listM = applicationContext.getBean(ListMapper.class);
-	ProductDetailMapper pdM = applicationContext.getBean(ProductDetailMapper.class);
+	WorkDetailMapper wdM = applicationContext.getBean(WorkDetailMapper.class);
 	
-	/*
-	System.out.println("4.선택한 작품의 포스터,상품 출력하기(보류:결과값이 다르게 출력됨)");
+	System.out.println("1.작품상세페이지 작품정보(작품이름,작품 포스터) 출력 ");
 	
-	Work work = mainM.selectByWork(1);
-	System.out.println(work);
-	
-//	List<Work> workList2 = mainM.selectByWork(1);
-//	for (Work work : workList2) {
-//		System.out.println("["+work.getwNo()+","+work.getwCategory()+","+work.getwPoster()+","+work.getwName()+","+work.getwTepisode()+"]");
-//		
-//	}
-	List<Product> p = work.getProduct();
-	for (Product product : p) {
-		System.out.println(product.getpNo()+product.getpName());
-	}
+	Work work = wdM.selectWorkOne(1);
+	System.out.println(work.getwPoster()+":"+work.getwName());
 	System.out.println("----------------------------------------");
 	
+	System.out.println("2.작품상세페이지 상품정보(상품이름, 상품에피소드,상품씬,작성자) 출력");
 	
-	System.out.println("5.선택한 작품의 포스터,해당 회차의 상품들 출력하기");
-	Map map = new HashMap();
-	map.put("w_no", 1);
-	map.put("wd_episode",1);
-	List<Work> w = mainM.selectByEpisode(map);
+	List<Work> w = listM.selectProductList(1);
+	List<Product> p = new ArrayList<Product>();
 	
 	for (int i = 0; i < w.size(); i++) {
-		System.out.println(w.get(i).getWdNo()+","+w.get(i).getwTepisode());
-		//System.out.println(w.get(i).getProduct());
+		System.out.println("에피소드:"+w.get(i).getWdEpisode());
+		p = w.get(i).getProduct();
+		for (int j = 0; j < p.size(); j++) {
+			System.out.println("product이름:"+p.get(j).getpName());
+			System.out.println("product이름:"+p.get(j).getpName());
+			System.out.println("장면(이미지):"+p.get(j).getPdScene());
+			System.out.println("작성자:"+p.get(j).getmId());
+			System.out.println("조회수:"+p.get(j).getpView());
+			System.out.println("관리자승인여부:"+p.get(j).getpCheck());
+		}
 	}
-	System.out.println("Product Scene: "+w.get(1).getProduct().get(0).getPdScene());
-	 */
-	/*
-		System.out.println("["+w.getWdNo()+","+w.getProduct().get(0).getpNo()+","+w.getwNo()+","+
-							w.getwTepisode()+","+w.getWdEpisode()+","+w.getwPoster()+","+w.getwName()+","+
-							w.getProduct().get(0).getpName()+","+w.getProduct().get(0).getPdScene()+"]");
 	System.out.println("----------------------------------------");
-	*/
+	
+	
+	System.out.println("3.작품상세페이지 상품정보 회차별 출력");
+	Map map2 = new HashMap();
+	map2.put("wNo", 1);
+	map2.put("wdEpisode", 1);
+	List<Work> w1 = listM.selectProductListByEpisode(map2);
+	List<Product> p2 = new ArrayList<Product>();
+	
+	for (int i = 0; i < w1.size(); i++) {
+		System.out.println("에피소드:"+w1.get(i).getWdEpisode());
+		p2 = w1.get(i).getProduct();
+		for (int j = 0; j < p2.size(); j++) {
+			System.out.println("product이름:"+p2.get(j).getpName());
+			System.out.println("product이름:"+p2.get(j).getpName());
+			System.out.println("장면(이미지):"+p2.get(j).getPdScene());
+			System.out.println("작성자:"+p2.get(j).getmId());
+			System.out.println("조회수:"+p2.get(j).getpView());
+			System.out.println("관리자승인여부:"+p2.get(j).getpCheck());
+			System.out.println();
+		}
+	}
+	System.out.println("----------------------------------------");
 	
 	/*
-	System.out.println("6.즐겨찾기 등록하기");
+	System.out.println("4.즐겨찾기 등록");
 	Map map = new HashMap();
 	map.put("mNo", 1);
 	map.put("pNo", 1);
-	int result = mainM.createBookmark(map);
+	int result = listM.createBookmark(map);
 	System.out.println(result);
 	System.out.println("----------------------------------------");
-	*/
-	/*
-	System.out.println("7.즐겨찾기 제거하기");
-	int result1 = mainM.deleteBookmark(1);
+	
+	
+	System.out.println("5.즐겨찾기 제거");
+	int result1 = listM.deleteBookmark(1);
 	System.out.println(result1);
 	System.out.println("----------------------------------------");
-
-	System.out.println("8.총회차 찾기");
-	int t = mainM.selectTepisode(1);
-	System.out.println(t);
-	System.out.println("----------------------------------------");
 	*/
+	
+	System.out.println("6.게시물 총 건수");
+	int cnt = listM.selectProductCount();
+	System.out.println(cnt);
+	
+	}
 }
