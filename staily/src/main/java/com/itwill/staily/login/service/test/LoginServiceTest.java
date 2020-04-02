2bfa45071412679578c2fe512cfe009ebfcd9ab6
yeongju-chun <yeongju30@naver.com>
2020-04-02 18:00:29 +0900

    LoginServiceTest 완료

--- staily/src/main/java/com/itwill/staily/login/service/test/LoginServiceTest.java
+++ staily/src/main/java/com/itwill/staily/login/service/test/LoginServiceTest.java
@@ -2,12 +2,10 @@ package com.itwill.staily.login.service.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
-
-import java.util.HashMap;
 
 import javax.sql.DataSource;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,11 +14,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.itwill.staily.login.mapper.LoginCommonMapper;
-import com.itwill.staily.login.mapper.LoginMapper;
-import com.itwill.staily.login.mapper.SignUpMapper;
 import com.itwill.staily.login.service.LoginService;
+import com.itwill.staily.util.Company;
 import com.itwill.staily.util.Member;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:spring/application-config.xml")
@@ -29,19 +26,10 @@ public class LoginServiceTest {
 	@Autowired
 	private ApplicationContext applicationContext;
 	@Autowired
-	private DataSource apacheDataSource;
-	@Autowired
-	private LoginMapper loginMapper;
-	@Autowired
-	private SignUpMapper signMapper;	
-	@Autowired
-	private LoginCommonMapper loginCommonMapper;
-	@Autowired
 	private LoginService loginService;
 	@Test
 	public void loginServiceTest() {
 		assertNotNull(applicationContext);
-		assertNotNull(apacheDataSource);
 		assertNotNull(loginService);
 	}
 	
@@ -51,37 +39,61 @@ public class LoginServiceTest {
 		member.setmId("hiphopmy");
 		member.setmPw("1234");
 		
-		assertEquals("hiphopmy", loginService.login(member));
+		assertEquals(3, loginService.login(member).getmNo());
 	}
 	
 	@Test
-	
 	public void findId() {
-		assertEquals(0,loginCommonMapper.isExistedId("hiphopmydsfadf"));
+		Assert.assertEquals("ehdanf91", loginService.findId("01091434908", "천영주"));
 	}
 	
 	@Test
-	@Ignore
 	public void isIdExist() {
-
+		assertEquals(1, loginService.isIdExist("ehdanf91", "01091434908"));
 	}
 	
 	@Test
-	@Ignore
 	public void updatePw() {
+		Member updateMember = new Member();
+		updateMember.setmId("ehdanf91");
+		updateMember.setmPw("1030");
 		
+		assertEquals(1, loginService.updatePw(updateMember));
 	}
 	
 	@Test
 	@Ignore
 	public void signMember() {
-		
+		Member member = new Member();
+		member.setmId("제이유닛테스뚜아이디");
+		member.setmPw("1111");
+		member.setmAddress("주소없음");
+		member.setmDaddress("디테일주소없음");
+		member.setmEmail("sdfhjs@sdkjfhdsfj");
+		member.setmName("이름짓기구찮");
+		member.setmPhone("01011112222");
+		member.setmType("M");
+		assertEquals(1, loginService.signMember(member));
 	}
 	
 	@Test
 	@Ignore
 	public void signCompany() {
+		Member member = new Member();
+		member.setmId("제이유닛테스뚜기업");
+		member.setmPw("1111");
+		member.setmAddress("기업주소없음");
+		member.setmDaddress("기업디테일주소없음");
+		member.setmEmail("giup@sdkjfhdsfj");
+		member.setmName("기업이름");
+		member.setmPhone("01099998888");
+		member.setmType("C");
 		
+		Company company = new Company();
+		//company.setCoNo(12345678);
+		
+		member.setmCompany(company);
+		assertEquals(1, loginService.signCompany(member));
 	}
 	
 	
