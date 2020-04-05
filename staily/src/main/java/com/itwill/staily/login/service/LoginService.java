b7dfed58be96013eb1d5183a095a15ba9b852c27
yeongju-chun <yeongju30@naver.com>
2020-04-05 15:40:06 +0900

    ..

--- staily/src/main/java/com/itwill/staily/login/service/LoginService.java
+++ staily/src/main/java/com/itwill/staily/login/service/LoginService.java
@@ -12,7 +12,7 @@ public interface LoginService {
 	int isIdExist(String mId, String mPhone);
 	//맞으면 새비밀번호 입력창으로 이동 
 
-	//새 비밀번호 입력창에서 입력된 비밀번호로 업데이트 
+	//새 비밀번호 입력창에서 입력된 비밀번호로 업데이트  
 	int updatePw(Member updatePwMember);
 
 	/******************************************************************/
