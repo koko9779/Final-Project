ce078ec7d09e47d46010f0e5e7ec62c17f176b28
sangsu <sangsu@DESKTOP-SMP4VOS>
2020-04-05 17:57:51 +0900

    Controller Test

--- staily/src/main/java/com/itwill/staily/mypage/controller/MypageController.java
+++ staily/src/main/java/com/itwill/staily/mypage/controller/MypageController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.itwill.staily.mypage.model.dto.Bookmark;
+import com.itwill.staily.mypage.model.dto.Friend;
 import com.itwill.staily.mypage.model.dto.Message;
 import com.itwill.staily.mypage.service.BookmarkService;
 import com.itwill.staily.mypage.service.FriendService;
@@ -28,7 +29,6 @@ public class MypageController {
 	
 	@Autowired
 	private MessageService messageService;
-	
 	@Autowired
 	private FriendService friendService;
 	@Autowired
@@ -41,7 +41,7 @@ public class MypageController {
 	@RequestMapping("/member_select")
 	public String member_select(HttpSession session, Model model)throws Exception{
 		//int mNo = (Integer)session.getAttribute("mNo");
-		int mNo = 7;
+		int mNo = 1;
 		Member member = mypageService.selectOne(mNo);
 		
 		if(member.getmType().equals("M")) {
@@ -65,21 +65,56 @@ public class MypageController {
 	
 	//북마크리스트
 	@RequestMapping("/bookmark_list")
-	public ModelAndView bookmark_list(HttpSession session) throws Exception{
-		ModelAndView mv = new ModelAndView();
-		List<Bookmark> bookmarkList = bookmarkService.selectList(1);
-		session.setAttribute("bookmarkList", bookmarkList);
-		mv.setViewName("test2");
-		return mv;
+	public String bookmark_list(@RequestParam int mNo, HttpServletRequest request) throws Exception{
+		//int mNo = (Integer)request.getAttribute("mNo");
+		List<Bookmark> bookmarkList = bookmarkService.selectList(mNo);
+		request.setAttribute("bookmarkList", bookmarkList);
+		
+		return "test2";
 	}
 	
-	//내가쓴글리스트
-	@RequestMapping("/member_write")
-	public String member_write(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+	//북마크삭제
+	@RequestMapping("/bookmark_delete")
+	public String bookmark_delete(@RequestParam int bmNo, Model model) throws Exception{
+		boolean result = bookmarkService.deleteBookmark(bmNo);
+		model.addAttribute("result", result);
+		return "forward:/bookmark_list";
+	}
+	
+	//친구리스트
+	@RequestMapping("/friend_list")
+	public String friend_list(HttpServletResponse response, HttpServletRequest request) throws Exception{
 		int mNo = (Integer)request.getAttribute("mNo");
-		List<Product> writeList = mypageService.selectWriteList(mNo);
-		//model.addAttribute("writeList", writeList);
-		request.setAttribute("writeList", writeList);
+		List<Friend> friendList = friendService.selectList(mNo);
+		request.setAttribute("friendList", friendList);
+		return "test2";
+	}
+	
+	//친구추가
+	@RequestMapping("/friend_create")
+	public String friend_create(HttpServletResponse response, HttpServletRequest request) throws Exception{
+		int fNo = (Integer)request.getAttribute("fNo");
+		int mNo = (Integer)request.getAttribute("mNo");
+		boolean result = friendService.createFriend(mNo, fNo);
+		request.setAttribute("result", result);
+		return "test2";
+	}
+	
+	//친구삭제
+	@RequestMapping("/friend_delete") 
+	public String friend_delete(HttpServletResponse response, HttpServletRequest request) throws Exception{
+		int fPk = (Integer)request.getAttribute("fPk");
+		boolean result = friendService.deleteFriend(fPk);
+		request.setAttribute("result", result);
+		return "test2";
+	}
+	
+	//친구찾기 -- RestController로 이동필요
+	@RequestMapping("/friend_find")
+	public String friend_find(HttpServletResponse response, HttpServletRequest request) throws Exception{
+		String mId = request.getParameter("mId");
+		String result = friendService.findFriend(mId);
+		request.setAttribute("result", result);
 		return "test2";
 	}
 	
@@ -94,14 +129,38 @@ public class MypageController {
 		return mv;
 	}
 	
-	//메시지리스트
+	//메시지리스트(멤버&메시지 조인)
 	@RequestMapping("/message_list")
 	public ModelAndView message_selectList(Model model) throws Exception{
 		ModelAndView mv = new ModelAndView();
 		List<Message> messageList = messageService.selectMessageList(2);
 		model.addAttribute("messageList", messageList);
-		mv.setViewName("test3");
+		mv.setViewName("test2");
 		return mv;
+	}
+	
+	//메시지 추가
+	@RequestMapping("/message_create")
+	public String message_create(HttpServletResponse response, HttpServletRequest request, Model model) throws Exception {
+		
+		return "";
+	}
+	
+	//메시지 삭제
+	@RequestMapping("/message_delete")
+	public String message_delete(HttpServletResponse response, HttpServletRequest request) throws Exception{
+		int msNo = (Integer)request.getAttribute("msNo");
+		return "";
+	}
+	
+	//내가쓴글리스트
+	@RequestMapping("/member_write")
+	public String member_write(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+		int mNo = (Integer)request.getAttribute("mNo");
+		List<Product> writeList = mypageService.selectWriteList(mNo);
+		//model.addAttribute("writeList", writeList);
+		request.setAttribute("writeList", writeList);
+		return "test2";
 	}
 	
 	//@RequestMapping("/test4")
